216e84613e185daa1724447e9fe4500793a527a9
Cho <chospace7@gmail.com>
2020-07-10 21:14:01 +0700

    color-flipper

---
+++ color-flipper/app.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "rgba(133,122,200)","#f15025"];
+const btn = document.getElementById("btn");
+const colors = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomNumber();
+})
