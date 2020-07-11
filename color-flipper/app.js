81d42d61c74942ba88f40a9f21598627c12f5cf4
Cho <chospace7@gmail.com>
2020-07-11 11:22:37 +0700

    app-js

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,7 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)","#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
-const colors = document.querySelector(".color");
+const color = document.querySelector(".color");
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function () {
     const randomNumber = getRandomNumber();
-})
+    // console.log(document.body);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
